00458cf23eae320306ad673f98f35f0d9297e020
Jon Sorbie <jon.sorbie@gmail.com>
2022-09-18 10:28:10 -0500

    Spring initializr files

---
+++ src/main/java/sorbie/jon/springwebopenapi/SpringWebOpenapiApplication.java
@@ -0,0 +1,13 @@
+package sorbie.jon.springwebopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebOpenapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sorbie/jon/springwebopenapi/SpringWebOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sorbie.jon.springwebopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
